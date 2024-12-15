cb8a2c80d9f8df87cd442371f889b101dc43dff2
amanglassfrog <135016564+amanglassfrog@users.noreply.github.com>
2024-12-15 08:16:06 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ["latin"], // Add more subsets if needed
@@ -29,6 +28,19 @@ export default function RootLayout({ children }) {
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-6K59SB18FT');
+            `,
+          }}
+        />
+        {/* Microsoft Clarity Tag */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "pdtndu8jye");
             `,
           }}
         />
